ce28c433b813f4984788057625ea38ecb877d981
ukarpenkov <truloveu@gmail.com>
2024-12-23 16:10:15 +0300

    orm nodejs

---
+++ 10_orm/index.js
@@ -0,0 +1,14 @@
+const Sequelize = require('sequelize')
+
+module.exports = function (sequelize) {
+    return sequelize.define("Mma",
+        
+    
+    )
+}
+
+
+// ['require', 'dependency'],
+// function (require, factory) {
+//     'use strict'
+// }
